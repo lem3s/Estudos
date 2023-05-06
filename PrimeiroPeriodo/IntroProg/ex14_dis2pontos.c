0a1235f4c8114c277d57d48fcbdac796b7936c2d
Lem3s <joaovictorlemesfaria@gmail.com>
2023-05-06 18:09:55 -0300

    feito o ex14

--- PrimeiroPeriodo/IntroProg/ex14_dis2pontos.c
+++ PrimeiroPeriodo/IntroProg/ex14_dis2pontos.c
@@ -1,8 +1,15 @@
+#include <math.h>
 #include <stdio.h>
 
 int main(){
-  float x1, y1, x2, y2;
+  float x1, y1, x2, y2, res;
+  scanf("%f", &x1);
+  scanf("%f", &y1);
+  scanf("%f", &x2);
+  scanf("%f", &y2);
   
-  
+  res = sqrt(pow(x1-x2, 2) + (pow(y1-y2, 2)));
+
+  printf("A DISTANCIA ENTRE A e B = %.2f\n", res);
   return 0;
 }
