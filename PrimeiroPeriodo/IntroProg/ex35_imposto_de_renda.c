46141198dba3e51ea005f7764c3a0156a82588f4
Lem3s <joaovictorlemesfaria@gmail.com>
2023-05-24 22:22:10 -0300

    fiz o ex35

--- PrimeiroPeriodo/IntroProg/ex35_imposto_de_renda.c
+++ PrimeiroPeriodo/IntroProg/ex35_imposto_de_renda.c
@@ -2,19 +2,35 @@
 
 int main(){
   int matricula, dependentes;
-  double sal_min, sal_fun, taxa, imposto;
+  double sal_min, sal_fun, taxa, imp_bruto, imp_liq, dif;
 
   scanf("%d %lf %d %lf %lf", &matricula, &sal_min, &dependentes, &sal_fun, &taxa);
 
-  if (sal_fun > sal_min*12){
-    imposto = sal_fun*0.2;
-  }else if (sal_fun > sal_min*5){
-    imposto= sal_fun*0.08;
+  if (sal_fun > 12*sal_min){
+    imp_bruto = sal_fun*0.2;
+
+  }else if (sal_fun > 5*sal_min){
+    imp_bruto= sal_fun*0.08;
+
   }else{
-    imposto = 0;
+    imp_bruto = 0;
   }
+  
+  imp_liq = imp_bruto - dependentes * 300;
+  dif = imp_liq - (sal_fun * (taxa / 100));
 
+  printf("MATRICULA = %d\n", matricula);
+  printf("IMPOSTO BRUTO = %.2lf\n", imp_bruto);
+  printf("IMPOSTO LIQUIDO = %.2lf\n", imp_liq);
+  printf("RESULTADO = %.2lf\n", dif);
 
+  if (dif < 0){
+    printf("IMPOSTO A RECEBER\n");
+  } else if (dif == 0){
+    printf("IMPOSTO QUITADO\n");
+  } else if (dif > 0){
+    printf("IMPOSTO A PAGAR\n");
+  }
   
   return 0;
 }
