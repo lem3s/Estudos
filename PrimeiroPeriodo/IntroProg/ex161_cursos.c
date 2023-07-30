f6651a305f9f9ea9dd9e43844748b096ccf626b2
Lem3s <joaovictorlemesfaria@gmail.com>
2023-07-30 18:10:55 -0300

    ex161

--- PrimeiroPeriodo/IntroProg/ex161_cursos.c
+++ PrimeiroPeriodo/IntroProg/ex161_cursos.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
 
-struct Curso {
+struct Cursos {
   int cod;
-  int val;
+  float val;
   char nome[100];
 };
+
+struct Aluno {
+  char nome[500];
+  int cod;
+  int cred;
+};
+
+int main(){
+  int nCursos, nAlunos, i=0, j=0;
+  int codAluno, indexCurso;
+
+  scanf("%d", &nCursos);
+
+  struct Cursos cursos[nCursos];
+
+  for(i=0; i<nCursos; i++){
+    scanf("%d", &cursos[i].cod);
+    scanf("%f", &cursos[i].val);
+    getchar();
+    fgets(cursos[i].nome, sizeof(cursos[i].nome), stdin);
+  }
+
+  /*
+  scanf("%d", &nAlunos);
+  struct Aluno alunos[nAlunos];
+
+  for(i=0; i<nAlunos; i++){
+    fgets(alunos[i].nome, sizeof(alunos[i].nome), stdin);
+    scanf("%d", &alunos[i].cod);
+    scanf("%d", &alunos[i].cred);
+    getchar();
+  }
+
+  for(i=0; i<nAlunos; i++){
+    codAluno = alunos[i].cod;
+
+    for(j=0; j<nCursos; j++){
+      if(cursos[j].cod == codAluno){
+        indexCurso = j;
+        break;
+      }
+    }
+
+    printf("Aluno(a): %s Curso: %s Num. Creditos: %d Valor Credito: %f Mensalidade: %lf", alunos[i].nome, cursos[indexCurso].nome, alunos[i].cred, cursos[indexCurso].val, alunos[i].cred*cursos[indexCurso].val);
+  }
+  */
+
+  return 0;
+}
